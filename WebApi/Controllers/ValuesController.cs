d34cce2bd8e4118703a5c313854b3f0f755fb1ae
Sundesh Mahesh Chawla <51902386+tavisca-schawla@users.noreply.github.com>
2019-08-14 11:53:14 +0530

    Added Jenkinsfile

--- WebApi/Controllers/ValuesController.cs
+++ WebApi/Controllers/ValuesController.cs
@@ -6,29 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
-    [Route("go/[controller]")]
-    [ApiController]
-    public class GreetingController : ControllerBase
-    {
-        [HttpGet("{salutation}")]
-        public ActionResult<string> Get(string salutation)
-        {
-            return salutation == "hi" ? "hello" : salutation == "hello" ? "hi" : "Invalid";
-        }
-
-    }
-
     [Route("api/[controller]")]
     [ApiController]
     public class HiHelloController : ControllerBase
-    {       
-        
+    {
+
         [HttpGet("{salutation}")]
         public ActionResult<string> Get(string salutation)
         {
             return salutation == "hi" ? "hello" : salutation == "hello" ? "hi" : "Invalid";
         }
 
-        
+
     }
 }
